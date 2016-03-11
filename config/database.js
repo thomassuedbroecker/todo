306471aaabf9b95b0361d2517675e2ad9eedc569
Lionel Mace <lionel.mace@fr.ibm.com>
2016-03-11 19:03:30 +0100

    Updated image in readme.

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	// Database url to connect
     // MongoLab DBaaS
-    //url : 'mongodb://todouser:todopwd@ds033285.mongolab.com:33285/todos'
+    url : 'mongodb://todouser:todopwd@ds033285.mongolab.com:33285/todos'
     // Mongo Locally
-    url : 'mongodb://localhost:27017/todo'
+    //url : 'mongodb://localhost:27017/todo'
 }
